067dec4443376c3b16ade78e59f14916519db648
yennguyen196 <yen2851996@gmail.com>
2018-07-31 09:31:29 +0700

    first

--- app/src/main/java/com/contact/yen/loadallcontact/MainActivity.java
+++ app/src/main/java/com/contact/yen/loadallcontact/MainActivity.java
@@ -1,13 +1,25 @@
 package com.contact.yen.loadallcontact;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mButtonShowContact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButtonShowContact = findViewById(R.id.button_load_contact);
+        mButtonShowContact.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, ContactActivity.class);
+        startActivity(intent);
+    }
+
 }
